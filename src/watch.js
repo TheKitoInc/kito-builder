27f085e69177c9ebf3e3ab40c4e5e60592c777e3
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-05-17 21:10:07 -0300

    Create watch.js

---
+++ src/watch.js
@@ -0,0 +1,16 @@
+import { watch } from "chokidar";
+
+const runWatch = function (src, callback) {
+  // Watch for changes in the source directory
+  watch(src).on("all", (event, path) => {
+    if (event === "add" || event === "change") {
+      // Process the file with the plugins
+      console.log(`File ${event}: ${path}`);
+
+      // Process the file with the plugins
+      callback(path);
+    }
+  });
+};
+
+export { runWatch };
