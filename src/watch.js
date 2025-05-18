7ee72ad4868b5de7b5b3fb417a77f47e15c4d0a5
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2025-05-17 21:10:57 -0300

    Update watch.js

--- src/watch.js
+++ src/watch.js
@@ -3,7 +3,7 @@ import { watch } from "chokidar";
 const runWatch = function (src, callback) {
   // Watch for changes in the source directory
   watch(src).on("all", (event, path) => {
-    if (event === "add" || event === "change") {
+    if (event === "change") {
       // Process the file with the plugins
       console.log(`File ${event}: ${path}`);
 
